daf6d2d1351bd03d5e2754476d44759744a33563
kguzik <karolina.guzik08@gmail.com>
2020-09-19 23:05:32 +0200

    app component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/styles.scss';
+import Header from './components/Header';
+import Slider from './components/Slider';
+import Footer from './components/Footer'
+import { FullPage, Slide } from 'react-full-page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FullPage controls={false}>
+        <Slide>
+          <Header title="Karolina Guzik" subtitle="Front-End Developer" text="I have 2 years of experience working as a Front-End Developer / Web Developer. I worked mainly with HTML5, Sass, Bootstrap, WordPress and JavaScript technologies. I am currently learning ReactJS,  on which I would like to focus the next step in my career."/>
+        </Slide>
+        <Slide>
+          <Slider title="My projects"/>
+        </Slide>
+        <Footer/>
+      </FullPage>
     </div>
   );
 }
