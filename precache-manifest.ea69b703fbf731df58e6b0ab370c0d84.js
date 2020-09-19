96b2f0cf4642328216576924428cfec94b092c93
kguzik <karolina.guzik08@gmail.com>
2020-09-19 23:23:54 +0200

    Updates

---
+++ precache-manifest.ea69b703fbf731df58e6b0ab370c0d84.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b121ddc5c9dbda94d16e60f5533f7f24",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "888b335df235ec9a7c1a",
+    "url": "/new-portfolio/static/css/2.25249b3f.chunk.css"
+  },
+  {
+    "revision": "0e235c5585030a49cde3",
+    "url": "/new-portfolio/static/css/main.12fc6180.chunk.css"
+  },
+  {
+    "revision": "888b335df235ec9a7c1a",
+    "url": "/new-portfolio/static/js/2.cb114d96.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/new-portfolio/static/js/2.cb114d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e235c5585030a49cde3",
+    "url": "/new-portfolio/static/js/main.9847aaba.chunk.js"
+  },
+  {
+    "revision": "e04cf1c6ca8475607064",
+    "url": "/new-portfolio/static/js/runtime-main.aea74c89.js"
+  },
+  {
+    "revision": "15ca2b4127a22fa42f5811cde2b01736",
+    "url": "/new-portfolio/static/media/consulting.15ca2b41.png"
+  },
+  {
+    "revision": "b509d25b13b5e90efbda7991d811da67",
+    "url": "/new-portfolio/static/media/jansbud.b509d25b.png"
+  },
+  {
+    "revision": "73daebddd5cc36a5b53f85dca47874c6",
+    "url": "/new-portfolio/static/media/ktory-kosz.73daebdd.png"
+  },
+  {
+    "revision": "42b35325a5b5029f77aec83243aff121",
+    "url": "/new-portfolio/static/media/me-blue.42b35325.png"
+  },
+  {
+    "revision": "f361fc054badae34d0e5807b64e15511",
+    "url": "/new-portfolio/static/media/movie-quotes-quiz.f361fc05.png"
+  },
+  {
+    "revision": "a4a3f42590e0c866c474b7c38ea34bed",
+    "url": "/new-portfolio/static/media/reveil.a4a3f425.png"
+  },
+  {
+    "revision": "2de295c2b7099e718a206e17d4a6c3d6",
+    "url": "/new-portfolio/static/media/tazz.2de295c2.png"
+  },
+  {
+    "revision": "1f99fee64d0d57120faa76de33212949",
+    "url": "/new-portfolio/static/media/weather-finder.1f99fee6.png"
+  }
+]);
